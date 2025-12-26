d27b1438bb97e6397aa69d6f3cdf81213e9822a4
DaviMoreira27 <davimsantana2706@gmail.com>
2025-12-25 21:39:16 -0300

    fix: always throwing browser opening error

--- scraper/src/scraper/scraper.service.ts
+++ scraper/src/scraper/scraper.service.ts
@@ -2,38 +2,51 @@ import * as browserApi from "playwright";
 import { type Buffer } from "buffer";
 
 export async function scrapePage(url: string): Promise<Buffer | undefined> {
+
+  let browser: browserApi.Browser;
+  let context: browserApi.BrowserContext;
+
   try {
-    const browser = await browserApi.chromium.launch();
-    const context = await browser.newContext({
+    browser = await browserApi.chromium.launch();
+    context = await browser.newContext({
       userAgent: `Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)`,
     });
-    try {
-      const page = await context.newPage();
+  } catch (error) {
+    console.error("Error starting the browser api", error);
+    throw new Error("Invalid browser environment");
+  }
 
-      const pageResponse = await page.goto(url, {
-        waitUntil: "domcontentloaded",
-        timeout: 2 * 60 * 60,
-      }); // 2 minutes
 
-      const pageScreenshot = await page.screenshot({ fullPage: true });
+  try {
+    const page = await context.newPage();
 
-      const success = pageResponse?.ok();
+    const pageResponse = await page.goto(url, {
+      waitUntil: "domcontentloaded",
+      timeout: 2 * 60 * 60,
+    }); // 2 minutes
 
-      if (success && calculateBufferSize(pageScreenshot) > 0) {
-        return pageScreenshot;
-      }
-    } catch (error) {
-      console.error(`Error scrapping page, ${url}`);
-      if (error instanceof browserApi.errors.TimeoutError) {
-        console.error("Timeout error recorded");
-      }
-    } finally {
-      await context.close();
-      await browser.close();
+    const pageScreenshot = await page.screenshot({ fullPage: true });
+
+    const success = pageResponse?.ok();
+
+    if (success && calculateBufferSize(pageScreenshot) > 0) {
+      return pageScreenshot;
     }
   } catch (error) {
-    console.error("Error starting the browser api");
-    throw new Error("Invalid browser environment");
+    console.error(`Error scrapping page, ${url}`);
+    if (error instanceof browserApi.errors.TimeoutError) {
+      console.error("Timeout error recorded");
+      throw new Error('Timeout error, could not access given page');
+    }
+    throw new Error('Page couldnt be accessed');
+  } finally {
+    if (context) {
+      await context.close();
+    }
+
+    if (browser) {
+      await browser.close();
+    }
   }
 }
 
