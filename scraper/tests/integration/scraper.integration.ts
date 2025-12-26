e6bcd2988f70b0d962c34e8cd911ba59d8a53321
DaviMoreira27 <davimsantana2706@gmail.com>
2025-12-25 21:39:29 -0300

    test: added integration tests for scrape page method

--- scraper/tests/integration/scraper.integration.ts
+++ scraper/tests/integration/scraper.integration.ts
@@ -1,12 +1,43 @@
-// import { describe, it } from "node:test";
-// import * as scraperService from "../../src/scraper/scraper.service.ts";
-// import assert from "node:assert";
+import { after, describe, it } from "node:test";
+import * as scraperService from "../../src/scraper/scraper.service.ts";
+import assert from "node:assert";
+import { readFile, unlink, writeFile } from "node:fs/promises";
 
-// describe("Page access", () => {
-//   it("Should work for a valid and accessible page", async () => {
-//     const page = "https://google.com";
-//     const data = await scraperService.getPage(page);
+const filePath = "./tests/integration/screenshot.png";
 
-//     assert.equal(data, true);
-//   });
-// });
+describe("scrapePage", () => {
+    it("should return a non-empty buffer for a valid page", async () => {
+        const page = "https://google.com"; page
+        const buffer = await scraperService.scrapePage(page);
+
+        assert.ok(buffer.length > 0);
+    });
+
+    it("should throw an error for an inaccessible page", async () => {
+        const id = crypto.randomUUID();
+        const inexistentPage = `https://non-existent-page-${id}.com`;
+
+        assert.rejects(async () => { await scraperService.scrapePage(inexistentPage) }, Error);
+    });
+
+    it("should handle real navigation timeouts", async () => {
+        const timeoutUrl = "http://10.255.255.1";
+
+        await assert.rejects(
+            scraperService.scrapePage(timeoutUrl),
+            Error
+        );
+    });
+
+    it("should download the screenshot of a valid page", async () => {
+        const page = "https://google.com";
+        const buffer = await scraperService.scrapePage(page);
+
+        await writeFile(filePath, buffer);
+
+        const savedFile = await readFile(filePath);
+        assert.ok(savedFile.length > 0);
+
+        await unlink(filePath);
+    });
+});
